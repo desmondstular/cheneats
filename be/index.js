0d4620249e2ae41effd185e063858a22e1953cb2
des267 <dstular@yahoo.com>
2024-02-29 13:27:23 -0700

    Added index.js to backend

--- be/index.js
+++ be/index.js
@@ -4,3 +4,19 @@
  * Starts backend application using express to connect
  * to a MongoDB database.
  */
+
+import express from "express";
+
+const port = 8000;
+const app = express();
+
+// Express built in body-parser
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+// Routes
+
+// Configure listening
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});
