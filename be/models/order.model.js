80d15f3327c6532f8cd1cfce7f2966e6458adcfe
des267 <dstular@yahoo.com>
2024-03-05 12:48:37 -0700

    Added menu and items in an order to their schemas

--- be/models/order.model.js
+++ be/models/order.model.js
@@ -11,7 +11,11 @@ const orderSchema = new mongoose.Schema(
         "id": {type: Number, required: true, unique: true},
         "customer_ref": {type: mongoose.Types.ObjectId, required: true, unique: true},
         "restaurant_ref": {type: mongoose.Types.ObjectId, required: true, unique: true},
-        "items": {type: mongoose.Types.Array, required: true},
+        "items": [{
+            "item_name": {type: String, required: true, unique: true},
+            "price": {type: Number, min: 0, required: true},
+            "quantity": {type: Number, min: 1, required: true}
+        }],
         "total": {type: Number, required: true},
         "status": {type: String, required: true, value: ["active", "pending", "complete", "cancelled"]}
     },
--- be/models/restaurant.model.js
+++ be/models/restaurant.model.js
@@ -10,9 +10,19 @@ const RestaurantSchema = new mongoose.Schema(
     {
         "id": {type: Number, required: true, unique: true},
         "email": {type: String, required: true, unique: true},
+        "password": {type: String, required: true},
         "name": {type: String, required: true},
         "location": {type: String, required: true},
-        "phoneNumber": {type: Number, required: true}
+        "phoneNumber": {type: Number, required: true},
+        "open_time": {type: String, required: true},
+        "closing_time": {type: String, required: true},
+        "taking_orders": {type: Boolean, required: true},
+        "menu": [{
+                "item_name": {type: String, required: true, unique: true},
+                "item_price": {type: Number, required: true, unique: true},
+                "item_image": {type: Number, required: true, unique: true},
+                "sold_out": {type: Boolean, required: true, unique: true}
+            }]
     },
     {strictQuery: true}
 );
