f1b525b49db252e28b83d7a44f32d1439ce6787e
des267 <dstular@yahoo.com>
2024-03-05 13:16:10 -0700

    Removed password from schema

--- be/models/restaurant.model.js
+++ be/models/restaurant.model.js
@@ -10,7 +10,6 @@ const RestaurantSchema = new mongoose.Schema(
     {
         "id": {type: Number, required: true, unique: true},
         "email": {type: String, required: true, unique: true},
-        "password": {type: String, required: true},
         "name": {type: String, required: true},
         "location": {type: String, required: true},
         "phoneNumber": {type: Number, required: true},
