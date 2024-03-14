d98fa96322dd2be52f16f483847955692c06d0cc
Desmond Stular <dstular@yahoo.com>
2024-03-14 12:13:14 -0600

    [FE] Setup vite environment

--- be/models/restaurant.model.js
+++ be/models/restaurant.model.js
@@ -11,9 +11,11 @@ const RestaurantSchema = new mongoose.Schema(
         "name": {type: String, required: true},
         "location": {type: String, required: true},
         "phone": {type: String, required: true},
+		"cuisine": {type: String, required: true},
         "open_time": {type: String},
         "closing_time": {type: String},
-        "accepting_orders": {type: Boolean}
+        "accepting_orders": {type: Boolean},
+		"image": {type: String, required: true}
     },
     {strictQuery: true}
 );
