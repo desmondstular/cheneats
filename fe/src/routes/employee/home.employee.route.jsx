c12a2ca632a375eb5aed09ec90cfb6b5c7836d08
Desmond Stular <dstular@yahoo.com>
2024-03-14 13:15:57 -0600

    [FE] Added employee side routes

---
+++ fe/src/routes/employee/home.employee.route.jsx
@@ -0,0 +1,17 @@
+/**
+ * home.employee.route.jsx
+ *
+ * The homepage route for restaurants.
+ */
+
+import React from "react";
+
+const RestaurantHome = () => {
+	return (
+		<div>
+
+		</div>
+	)
+};
+
+export default RestaurantHome;
