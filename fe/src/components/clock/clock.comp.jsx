c16de0f6ac573c45c0ed75825946159dbca70abb
Desmond Stular <dstular@yahoo.com>
2024-04-04 13:20:06 -0600

    [FE] Fixed clock digits

--- fe/src/components/clock/clock.comp.jsx
+++ fe/src/components/clock/clock.comp.jsx
@@ -7,6 +7,7 @@ import React, {useEffect, useState} from "react";
 export const Clock = () => {
 	const [currentTime, setCurrentTime] = useState(new Date());
 
+
 	useEffect(() => {
 		const timer = setInterval(() => {
 			setCurrentTime(new Date());
@@ -17,7 +18,7 @@ export const Clock = () => {
 	return (
 		<div className='rounded-box w-28 h-12 bg-base-100 shadow-sm flex justify-center items-center text-lg'>
 			<p className='font-medium font-sans'>
-				{currentTime.getHours()}:{currentTime.getMinutes()}:{String(currentTime.getSeconds()).padStart(2, '0')}
+				{String(currentTime.getHours()).padStart(2, '0')}:{String(currentTime.getMinutes()).padStart(2, '0')}:{String(currentTime.getSeconds()).padStart(2, '0')}
 			</p>
 		</div>
 	)
