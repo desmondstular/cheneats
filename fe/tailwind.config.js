f62b9dbb8362b83df5ad0bd76b7aa6486dd6ea12
Desmond Stular <dstular@yahoo.com>
2024-04-05 01:48:22 -0600

    [FE] Completed employee home ordering system page.

--- fe/tailwind.config.js
+++ fe/tailwind.config.js
@@ -2,13 +2,19 @@
 export default {
   content: [
 	  "./index.html",
-	  "./src/**/*.{js,ts,jsx,tsx}"
+	  "./src/**/*.{js,ts,jsx,tsx}",
+	  'node_modules/preline/dist/*.js'
   ],
   theme: {
-    extend: {},
+    extend: {
+		colors: {
+			'chen-blue': "#00b6ff"
+		}
+	},
   },
   plugins: [
-	  require("daisyui")
+	  require("daisyui"),
+	  require('preline/plugin')
   ],
 	daisyui: {
 	  themes: [
