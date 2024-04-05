24f877897f44e7e708a894a99c6429874802828a
Gabriel Jorand <gabrieljorand@gmail.com>
2024-04-05 02:18:35 -0600

    fixed the most god aweful CSS styling in custOrder

--- fe/src/components/customerSide/orderView/cart.customer.comp.jsx
+++ fe/src/components/customerSide/orderView/cart.customer.comp.jsx
@@ -13,40 +13,44 @@ const CartCustomerComp = ({ order, removeFromCart, openCheckoutModal }) => {
 
     }
     return (
-        <div className="card w-96 mt-4 position-fixed">
-            <div className="flex card-title justify-content-center ">
-                <h1>My Order</h1>
-            </div>
-            <div className="card-body">
-                <div className="h-40 overflow-x-auto">
-                    <table className='table drop-shadow-md'>
-                        <thead>
-                        <tr className="text-sm">
-                            <th className='bg-blue-300'>Name</th>
-                            <th>Quantity</th>
-                            <th>Subtotal</th>
-                            <th></th> {/* New column for delete button */}
-                        </tr>
-                        </thead>
-                        <tbody>
-                        {order.items.map((item, index) => (
-                            <tr key={index}>
-                                <td>{item.name}</td>
-                                <td>{item.quantity}</td>
-                                <td>{'$' + item.subtotal.toFixed(2)}</td>
-                                <td>
-                                    <IconButton aria-label="delete" size="small" onClick={() => removeFromCart(item)}>
-                                        <DeleteIcon fontSize="small" />
-                                    </IconButton>
-                                </td>
-                            </tr>
-                        ))}
-                        </tbody>
-                    </table>
+        <div
+        >
+            
+            <div className="card w-96 mt-4 pl-30">
+                <div className=" ">
+                    <h1>My Order</h1>
                 </div>
-                <h2>Order Total: ${order.total.toFixed(2)}</h2>
-                <div className="flex card-actions justify-center flex-column align-content-center">
-                    <button className="btn" onClick={openCheckoutModal} disabled={disabledBtn}>CheckOut</button>
+                <div className="card-body">
+                    <div className="h-40 overflow-x-auto">
+                        <table className='table drop-shadow-md'>
+                            <thead>
+                            <tr className="text-sm">
+                                <th className='bg-blue-300'>Name</th>
+                                <th>Quantity</th>
+                                <th>Subtotal</th>
+                                <th></th> {/* New column for delete button */}
+                            </tr>
+                            </thead>
+                            <tbody>
+                            {order.items.map((item, index) => (
+                                <tr key={index}>
+                                    <td>{item.name}</td>
+                                    <td>{item.quantity}</td>
+                                    <td>{'$' + item.subtotal.toFixed(2)}</td>
+                                    <td>
+                                        <IconButton aria-label="delete" size="small" onClick={() => removeFromCart(item)}>
+                                            <DeleteIcon fontSize="small" />
+                                        </IconButton>
+                                    </td>
+                                </tr>
+                            ))}
+                            </tbody>
+                        </table>
+                    </div>
+                    <h2>Order Total: ${order.total.toFixed(2)}</h2>
+                    <div className="flex card-actions justify-center flex-column align-content-center">
+                        <button className="btn" onClick={openCheckoutModal} disabled={disabledBtn}>CheckOut</button>
+                    </div>
                 </div>
             </div>
         </div>
--- fe/src/components/customerSide/orderView/ordermenucard.customer.comp.jsx
+++ fe/src/components/customerSide/orderView/ordermenucard.customer.comp.jsx
@@ -21,51 +21,53 @@ export const OrderMenuCardCustomerComp = ({ menuItem, addToCart }) => {
     };
 
     return (
-        <Card style={{ margin: '2px', opacity: menuItem.available ? 1 : 0.5 }}>
-            <Card.Body style={{display: 'flex', justifyContent: 'space-between', flexDirection: 'row'}}>
-                <Card.Img src={menuItem.image} style={{height: '80px', width: '80px'}}/>
-                <div className="flex flex-column">
-                    <Card.Text style={{fontWeight: 'bold'}}>{menuItem.name}</Card.Text>
-                    <Card.Text className="flex flex-column">
-                        <span style={{
-                            textDecoration: menuItem.on_special ? 'line-through' : 'none',
-                            color: menuItem.on_special ? 'red' : 'black'
-                        }}>
-                            ${menuItem.price}
-                        </span>
-                        {menuItem.on_special && <span> ${menuItem.discount.toFixed(2)}</span>} {/* Render discount price if on_special is true */}
-                    </Card.Text>
-                </div>
-                <div style={{
-                    display: 'flex',
-                    flexDirection: 'row',
-                    alignItems: 'center',
-                    justifyContent: 'space-around'
-                }}>
-                    <Button disabled={!menuItem.available} variant="dark"
-                            style={{height: '40px', width: '40px', marginRight: '4px'}} onClick={decreaseQty}>-</Button>
+        <div className="px-28 pb-1">
+            <Card style={{ margin: '2px', opacity: menuItem.available ? 1 : 0.5 }}>
+                <Card.Body style={{display: 'flex', justifyContent: 'space-between', flexDirection: 'row'}}>
+                    <Card.Img src={menuItem.image} style={{height: '80px', width: '80px'}}/>
+                    <div className="flex flex-column">
+                        <Card.Text style={{fontWeight: 'bold'}}>{menuItem.name}</Card.Text>
+                        <Card.Text className="flex flex-column">
+                            <span style={{
+                                textDecoration: menuItem.on_special ? 'line-through' : 'none',
+                                color: menuItem.on_special ? 'red' : 'black'
+                            }}>
+                                ${menuItem.price}
+                            </span>
+                            {menuItem.on_special && <span> ${menuItem.discount.toFixed(2)}</span>} {/* Render discount price if on_special is true */}
+                        </Card.Text>
+                    </div>
                     <div style={{
                         display: 'flex',
-                        flexDirection: 'column',
+                        flexDirection: 'row',
+                        alignItems: 'center',
+                        justifyContent: 'space-around'
+                    }}>
+                        <Button disabled={!menuItem.available} variant="dark"
+                                style={{height: '40px', width: '40px', marginRight: '4px'}} onClick={decreaseQty}>-</Button>
+                        <div style={{
+                            display: 'flex',
+                            flexDirection: 'column',
+                            alignItems: 'center',
+                            justifyContent: 'center',
+                            marginLeft: '10px'
+                        }}>
+                            <Card.Text>{qty}</Card.Text>
+                        </div>
+                        <Button variant="dark" disabled={!menuItem.available}
+                                style={{height: '40px', width: '40px', marginLeft: '12px'}} onClick={increaseQty}>+</Button>
+                    </div>
+                    <div style={{
+                        display: 'flex',
+                        flexDirection: 'row',
                         alignItems: 'center',
                         justifyContent: 'center',
-                        marginLeft: '10px'
+                        marginLeft: '8px'
                     }}>
-                        <Card.Text>{qty}</Card.Text>
+                        <Button variant="dark" onClick={handleAddToCart} disabled={!menuItem.available}>Add to Cart</Button>
                     </div>
-                    <Button variant="dark" disabled={!menuItem.available}
-                            style={{height: '40px', width: '40px', marginLeft: '12px'}} onClick={increaseQty}>+</Button>
-                </div>
-                <div style={{
-                    display: 'flex',
-                    flexDirection: 'row',
-                    alignItems: 'center',
-                    justifyContent: 'center',
-                    marginLeft: '8px'
-                }}>
-                    <Button variant="dark" onClick={handleAddToCart} disabled={!menuItem.available}>Add to Cart</Button>
-                </div>
-            </Card.Body>
-        </Card>
+                </Card.Body>
+            </Card>
+        </div>
     );
 };
